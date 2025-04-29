baeaa3448f3e2ef4870378d38ebac80151d7bfdf
Computação <luizfelipebuaszczyk@hotmail.com>
2025-04-29 20:19:26 -0300

    feat: Implementação de testes para criação do usuário
    -Verifica se usuário foi registrado com sucesso\n-Verifica se falhou ao tentar registrar sem o campo name

--- backend/tests/CreateUser.spec.js
+++ backend/tests/CreateUser.spec.js
@@ -12,5 +12,34 @@ describe('CreateUser', () => {
 
         response = await request(app).post('/register/').send(user);
 
-    })
+        // Valida se retornou 201 -- Gravou com sucesso
+        expect(response.status).toBe(201);
+
+        // Validando corpo de resposta do cadastro
+        expect(response.body).toHaveProperty('id_user');
+        expect(response.body.name).toBe(user.name);
+        expect(response.body.email).toBe(user.email);
+    });
+
+    it('should return 400 if name is missing', async () => {
+        const user = {
+            email: 'teste400@email.com',
+            password: 'teste1234'
+        };
+
+        response = await request(app).post('/register/').send(user);
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('messages');
+
+        // Verifica se o response body retornou -> field:'name'
+        expect(response.body.messages).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    field: 'name'
+                })
+            ])
+        )
+    });
+
 })
+
