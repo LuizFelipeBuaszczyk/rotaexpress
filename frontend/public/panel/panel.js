6b53329ca13e50af7ec47856bb9b86cd6381becf
Lucas Stasiak <101188@aluno.uricer.edu.br>
2025-05-25 21:46:57 -0300

    logout

--- frontend/public/panel/panel.js
+++ frontend/public/panel/panel.js
@@ -1,5 +1,5 @@
-import { logout } from "../Scripts/utils/logout.js";
-import { checkToken } from "../Scripts/utils/http.js";
+//import { logout } from "../Scripts/utils/logout.js";
+//import { checkToken } from "../Scripts/utils/http.js";
 
 document.querySelectorAll('[data-logout]').forEach(element => {
     element.addEventListener('click', function(e) {
@@ -12,7 +12,7 @@ document.getElementById('cancel-logout-button').addEventListener('click', () => 
     document.getElementById('logout-modal').style.display = 'none';
 });
 
-document.getElementById('confirm-logout-button').addEventListener('click', logout); 
+//document.getElementById('confirm-logout-button').addEventListener('click', logout); 
 
 window.onload = () => {
     login = checkToken();
